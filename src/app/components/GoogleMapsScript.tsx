64be072390729829e01e9bd2bd587d7fee602613
synopustech <kimtee92@gmail.com>
2025-08-03 00:15:24 +1000

    Update GoogleMapsScript.tsx

--- src/app/components/GoogleMapsScript.tsx
+++ src/app/components/GoogleMapsScript.tsx
@@ -5,7 +5,7 @@ import Script from 'next/script';
 export default function GoogleMapsScript() {
   return (
     <Script
-      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY}&libraries=places&v=weekly`}
+      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_PLACES_API_KEY}&libraries=places&v=weekly`}
       strategy="beforeInteractive"
       onLoad={() => {
         console.log('Google Maps API loaded successfully');
