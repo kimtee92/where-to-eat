c5e9b375ce9a511d4d27d7dc7afd4c12426e9cd2
synopustech <kimtee92@gmail.com>
2025-08-03 21:25:42 +1000

    async google apis

--- src/app/components/GoogleMapsScript.tsx
+++ src/app/components/GoogleMapsScript.tsx
@@ -88,7 +88,7 @@ export default function GoogleMapsScript() {
 
   return (
     <Script
-      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}&libraries=places&v=weekly`}
+      src={`https://maps.googleapis.com/maps/api/js?key=${process.env.NEXT_PUBLIC_GOOGLE_PLACES_API_KEY}&libraries=places&v=weekly&loading=async`}
       strategy="afterInteractive"
       onLoad={handleLoad}
       onError={(e) => {
